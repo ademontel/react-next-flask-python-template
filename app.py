5e64a7026efbe342b26be8fe5e1002069f2f43b8
Agustin Demontel <agustindemontel@gmail.com>
2025-01-11 08:44:51 -0300

    First commit, blank template

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route("/")
+def index():
+    return {"message": "Hello from Flask!"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
